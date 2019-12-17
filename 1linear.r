954be2297553f35d1d5502fc7434a2a75572d233
supriya <48164266+137priya@users.noreply.github.com>
2019-12-17 18:20:24 +0530

    Update 1linear.r

--- 1linear.r
+++ 1linear.r
@@ -27,9 +27,4 @@ lin_reg <- function(x, y, test) {
 }
 
 lin_reg(budget, sales, test)
-##################program3##################
-library(ISLR)
-library(class)
-data = Default
-data$default = ifelse(data$default == 'No', 0, 1)
-knn(data[,c(3, 4)], test = data.frame(balance = 10000, income=50000), cl = data$default, k = 3)
+
